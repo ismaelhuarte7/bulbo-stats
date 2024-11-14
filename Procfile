c7ab48d9a4cb97454abd2489344346d00f393174
ismaelhuarte7 <ismaelhuarte7@gmail.com>
2024-11-14 18:22:51 -0300

    config db azure

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -b 0.0.0.0:8000 src.web:app
+web: gunicorn -w 4 -b 0.0.0.0:8000 src.web:create_app()
--- src/config.py
+++ src/config.py
@@ -1,23 +1,25 @@
+import os
+
 class Config(object):
     SECRET_KEY = 'secret_key'
     TESTING = False
     SESSION_TYPE = 'filesystem'
-    
+
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/bulbostatsdb'
+    # Obtén la cadena de conexión desde las variables de entorno de Azure
+    connection_string = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING', '')
+    params = dict(item.split('=') for item in connection_string.split())
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{params['user']}:{params['password']}@{params['host']}:{params['port']}/{params['dbname']}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    pass
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/bulbostatsdb'
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
-    
-    pass
 
 config = {
-    "production" : ProductionConfig,
+    "production": ProductionConfig,
     "development": DevelopmentConfig,
 }
